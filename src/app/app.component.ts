9d10ab09a60e65060d93c27f54297083bcf56256
Strada Fabio <fabio.strada@allitude.it>
2022-02-22 12:52:11 +0100

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService, StateService, LanguageConfig } from 'my-authenticator-lib';
+import { EventService, StateService, LanguageConfig, BaseComponent } from 'my-authenticator-lib';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent extends BaseComponent implements OnInit {
   
   public property1!: string;
   public language!: string;
@@ -14,10 +15,16 @@ export class AppComponent implements OnInit {
 
   constructor(private stateService: StateService,
               private eventService: EventService) {
+      super();
   }
 
   ngOnInit(): void {
       this.property1 = this.stateService.getProperty1();
+      this.eventService.getLanguage()
+          .pipe(takeUntil(this.unsubscribeAll))
+          .subscribe(language => {
+              this.language = language;            
+          })
   }
 
   public selectLanguage() {
